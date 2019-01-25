8a0e129af2bc30b4d3275696e6d42c22c0d9b116
Daniel Higuero <daniel@daisho.group>
2019-01-25 10:15:25 +0100

    NP-661 Skeleton for device manager

---
+++ internal/pkg/entities/validation.go
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C)  2019 Nalej - All Rights Reserved
+ */
+
+package entities
+
+import (
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-device-go"
+	"github.com/nalej/grpc-device-manager-go"
+	"github.com/nalej/grpc-organization-go"
+)
+
+const emptyOrganizationId = "organization_id cannot be empty"
+const emptyDeviceGroupId = "device_group_id cannot be empty"
+const emptyDeviceId = "device_id cannot be empty"
+const emptyName = "name cannot be empty"
+const emptyDeviceGroupApiKey = "device_group_api_key cannot be empty"
+const emptyLabels = "labels cannot be empty"
+
+func ValidOrganizationID(organizationID *grpc_organization_go.OrganizationId) derrors.Error {
+	if organizationID.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	return nil
+}
+
+func ValidDeviceGroupID(deviceGroupID *grpc_device_go.DeviceGroupId) derrors.Error {
+	if deviceGroupID.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if deviceGroupID.DeviceGroupId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupId)
+	}
+	return nil
+}
+
+func ValidDeviceID(deviceId *grpc_device_go.DeviceId) derrors.Error {
+	if deviceId.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if deviceId.DeviceGroupId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupId)
+	}
+	if deviceId.DeviceId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceId)
+	}
+	return nil
+}
+
+func ValidAddDeviceGroupRequest(request *grpc_device_manager_go.AddDeviceGroupRequest) derrors.Error {
+	if request.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if request.Name == "" {
+		return derrors.NewInvalidArgumentError(emptyName)
+	}
+	return nil
+}
+
+func ValidUpdateDeviceGroupRequest(request *grpc_device_manager_go.UpdateDeviceGroupRequest) derrors.Error {
+	if request.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if request.DeviceGroupId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupId)
+	}
+	if !request.UpdateEnabled && !request.UpdateDeviceConnectivity {
+		return derrors.NewInvalidArgumentError("either update_enabled or update_device_connectivity must be set")
+	}
+	return nil
+}
+
+func ValidRegisterDeviceRequest(request *grpc_device_manager_go.RegisterDeviceRequest) derrors.Error {
+	if request.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if request.DeviceGroupId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupId)
+	}
+	if request.DeviceGroupApiKey == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupApiKey)
+	}
+	if request.DeviceId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceId)
+	}
+
+	return nil
+}
+
+func ValidDeviceLabelRequest(request *grpc_device_manager_go.DeviceLabelRequest) derrors.Error {
+	if request.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if request.DeviceGroupId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupId)
+	}
+	if request.DeviceId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceId)
+	}
+	if len(request.Labels) == 0 {
+		return derrors.NewInvalidArgumentError(emptyLabels)
+	}
+
+	return nil
+}
+
+func ValidUpdateDeviceRequest(request *grpc_device_manager_go.UpdateDeviceRequest) derrors.Error {
+	if request.OrganizationId == "" {
+		return derrors.NewInvalidArgumentError(emptyOrganizationId)
+	}
+	if request.DeviceGroupId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceGroupId)
+	}
+	if request.DeviceId == "" {
+		return derrors.NewInvalidArgumentError(emptyDeviceId)
+	}
+
+	return nil
+}
+
---
+++ internal/pkg/server/service.go
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2019 Nalej - All Rights Reserved
+ */
+
+package server
+
+import (
+	"github.com/nalej/device-manager/internal/pkg/server/device"
+	"github.com/nalej/grpc-authx-go"
+	"github.com/nalej/grpc-device-go"
+	"github.com/nalej/grpc-device-manager-go"
+	"github.com/nalej/grpc-utils/pkg/tools"
+	"fmt"
+	"github.com/nalej/derrors"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+// Service structure with the configuration and the gRPC server.
+type Service struct {
+	Configuration Config
+	Server * tools.GenericGRPCServer
+}
+
+// NewService creates a new system model service.
+func NewService(conf Config) *Service {
+	return &Service{
+		conf,
+		tools.NewGenericGRPCServer(uint32(conf.Port)),
+	}
+}
+
+// Clients structure with the gRPC clients for remote services.
+type Clients struct {
+	AuthxClient grpc_authx_go.AuthxClient
+	DevicesClient grpc_device_go.DevicesClient
+}
+
+// GetClients creates the required connections with the remote clients.
+func (s * Service) GetClients() (* Clients, derrors.Error) {
+	authxConn, err := grpc.Dial(s.Configuration.AuthxAddress, grpc.WithInsecure())
+	if err != nil{
+		return nil, derrors.AsError(err, "cannot create connection with the authx component")
+	}
+
+	smConn, err := grpc.Dial(s.Configuration.SystemModelAddress, grpc.WithInsecure())
+	if err != nil{
+		return nil, derrors.AsError(err, "cannot create connection with the system model component")
+	}
+
+	aClient := grpc_authx_go.NewAuthxClient(authxConn)
+	dClient := grpc_device_go.NewDevicesClient(smConn)
+
+	return &Clients{aClient, dClient}, nil
+}
+
+// Run the service, launch the REST service handler.
+func (s *Service) Run() error {
+	cErr := s.Configuration.Validate()
+	if cErr != nil{
+		log.Fatal().Str("err", cErr.DebugReport()).Msg("invalid configuration")
+	}
+	s.Configuration.Print()
+	clients, cErr := s.GetClients()
+	if cErr != nil{
+		log.Fatal().Str("err", cErr.DebugReport()).Msg("Cannot create clients")
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.Configuration.Port))
+	if err != nil {
+		log.Fatal().Errs("failed to listen: %v", []error{err})
+	}
+
+	// Create handlers
+	manager := device.NewManager(clients.AuthxClient, clients.DevicesClient)
+	handler := device.NewHandler(manager)
+
+	grpcServer := grpc.NewServer()
+
+	grpc_device_manager_go.RegisterDevicesServer(grpcServer, handler)
+
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
+	log.Info().Int("port", s.Configuration.Port).Msg("Launching gRPC server")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal().Errs("failed to serve: %v", []error{err})
+	}
+	return nil
+}
